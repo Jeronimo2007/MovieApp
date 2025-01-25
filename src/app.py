4be9cd18d6454da259b4434f02aa4004fe448240
jeronimo0109 <jeronimogomez0107@gmail.com>
2025-01-24 20:48:27 -0500

    finished login

--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, render_template, request,redirect,url_for,flash
 from flask_mysqldb import MySQL 
+from flask_wtf.csrf import CSRFProtect
+from flask_login import LoginManager, login_user, logout_user, login_required
 
 from config import config
 
@@ -11,7 +13,14 @@ from models.entities.User import User
 
 app = Flask(__name__)
 
+csrf = CSRFProtect()
 db = MySQL(app)
+login_manager_app = LoginManager(app)
+
+@login_manager_app.user_loader
+def load_user(id):
+    return ModelUser.get_by_id(db,id)
+
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
@@ -22,6 +31,7 @@ def login():
         logged_User = ModelUser.login(db,user)
         if logged_User != None:
             if logged_User.password:
+                login_user(logged_User)
                 return redirect(url_for('home'))
             else:
                 flash('Invalid password')
@@ -34,6 +44,23 @@ def login():
         return render_template('auth/login.html')
     
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+    
+
+@app.route('/protected')
+@login_required
+def protected():
+    return "<h1> ESTA ES UNA VISTA PROTEGIDA SOLO PARA USUARIOS</h1>"
+
+def status_401(error):
+    return redirect(url_for('login'))
+
+def status_404(error):
+    return '<h1>Pagina no encontrada</h1?', 404
+
 @app.route('/home')
 def home():
     return render_template('home.html')
@@ -45,4 +72,7 @@ def index():
 
 if __name__ == '__main__':
     app.config.from_object(config['development'])
+    csrf.init_app(app)
+    app.register_error_handler(401, status_401)
+    app.register_error_handler(404, status_404)
     app.run()
